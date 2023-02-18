6537474c16c7fbfd3165c5522e331a32a49cdcdd
Joseph Onyach <josephokothonyach@gmail.com>
2023-02-17 16:12:04 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 
-/
+/*
 *main - Entry point
 *Return:Always 0 (success)
 */
@@ -10,7 +10,8 @@
 
 int main(void)
 
-{puts("\"Programming is like building a multilingual puzzle");
+{
+  puts("\"Programming is like building a multilingual puzzle");
 
 	return (0)
 
